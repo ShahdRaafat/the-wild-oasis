f9b4ba7a5756d3b53255d1bb1c432c8fc1ecbbc2
ShahdRaafat <shahdraafat.725@gmail.com>
2025-08-07 23:55:37 +0300

    feat: Create reusable UI components: Button, Heading, Input, and Row

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Heading from "./ui/Heading";
+import Input from "./ui/Input";
+import Row from "./ui/Row";
+
+const StyledApp = styled.main`
+  padding: 30px;
+`;
 function App() {
-    return (
-        <div>
-            Hello
-        </div>
-    )
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row type="horizontal">
+          <Heading as="h1">The wild oasis</Heading>
+          <div>
+            <Heading as="h2">Check in and out</Heading>
+            <Button>check in</Button>
+            <Button size="large" variation="primary">
+              check out
+            </Button>
+          </div>
+        </Row>
+        <Row>
+          <Heading as="h3">Form</Heading>
+          <div>
+            <Input type="number" placeholder="shosho" />
+            <Input type="number" placeholder="shosho" />
+          </div>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
-export default App
+export default App;
