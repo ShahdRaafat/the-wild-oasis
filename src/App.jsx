e3f80c21ef25bdd207675e5abdd68720866b08b4
ShahdRaafat <shahdraafat.725@gmail.com>
2025-08-10 21:45:47 +0300

    feat: Integrate Supabase for cabin data retrieval and add React Query for state management

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,21 @@ import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 import { SidebarProvider } from "./contexts/SidebarContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <SidebarProvider>
         <GlobalStyles />
         <BrowserRouter>
@@ -33,7 +45,7 @@ function App() {
           </Routes>
         </BrowserRouter>
       </SidebarProvider>
-    </>
+    </QueryClientProvider>
   );
 }
 
