90000f5a0bf909905705576bad15d27296f8c006
ShahdRaafat <shahdraafat.725@gmail.com>
2025-08-16 21:22:52 +0300

    feat: Close modal when clicked outside

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -3,6 +3,7 @@ import { device } from "../styles/breakpoints";
 import { HiXMark } from "react-icons/hi2";
 import { createPortal } from "react-dom";
 import { cloneElement, createContext, useContext, useState } from "react";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -77,10 +78,13 @@ function Open({ children, opens: openWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
+
   if (name !== openName) return null;
+
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
