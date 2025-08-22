a843de7ea7e9b6723ad0e664d99b6b8246785c26
ShahdRaafat <shahdraafat.725@gmail.com>
2025-08-22 21:06:40 +0300

    feat: Add Checkin page with CheckinBooking component and related hooks

--- src/features/bookings/useBooking.js
+++ src/features/bookings/useBooking.js
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 export function useBooking() {
   const { bookingId } = useParams();
   const { data: booking, isPending } = useQuery({
-    queryKey: ["booking"],
+    queryKey: ["booking", bookingId],
     queryFn: () => getBooking(bookingId),
     retry: false,
   });
--- src/pages/Checkin.jsx
+++ src/pages/Checkin.jsx
@@ -1,5 +1,7 @@
+import CheckinBooking from "../features/check-in-out/CheckinBooking";
+
 function Checkin() {
-  return <div></div>;
+  return <CheckinBooking />;
 }
 
 export default Checkin;
