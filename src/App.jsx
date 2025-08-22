fe2222e7b2dc6cfb420282f87ca15272d69a93b5
ShahdRaafat <shahdraafat.725@gmail.com>
2025-08-23 02:38:00 +0300

    feat: Add ProtectedRoute component and implement user authorization flow

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,17 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
-
+import Booking from "./pages/Booking";
+import Checkin from "./pages/Checkin";
 import GlobalStyles from "./styles/GlobalStyles";
+
 import AppLayout from "./ui/AppLayout";
+import ProtectedRoute from "./ui/ProtectedRoute";
+
 import { SidebarProvider } from "./contexts/SidebarContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
-import Booking from "./pages/Booking";
-import Checkin from "./pages/Checkin";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -33,7 +35,13 @@ function App() {
         <GlobalStyles />
         <BrowserRouter>
           <Routes>
-            <Route element={<AppLayout />}>
+            <Route
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="dashboard" />} />
               <Route path="Dashboard" element={<Dashboard />}></Route>
               <Route path="bookings" element={<Bookings />}></Route>
