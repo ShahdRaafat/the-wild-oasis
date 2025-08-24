8da8b74688f14c7939f47b7658ef42d5121451ab
ShahdRaafat <shahdraafat.725@gmail.com>
2025-08-24 17:03:14 +0300

    feat: Add user account management features including password update and user data form

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,11 +2,22 @@ import styled from "styled-components";
 import { TfiMenu } from "react-icons/tfi";
 import { useIsTablet } from "../hooks/useIsTablet";
 import { useSidebar, SidebarProvider } from "../contexts/SidebarContext";
-import Logout from "../features/authentication/Logout";
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar";
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  @media (max-width: 900px) {
+    justify-content: space-between;
+  }
+`;
+const RightContainer = styled.div`
+  display: flex;
+  gap: 2.4rem;
 `;
 function Header() {
   const isTablet = useIsTablet();
@@ -19,7 +30,10 @@ function Header() {
           <TfiMenu onClick={toggleSidebar} />
         </span>
       )}
-      <Logout />
+      <RightContainer>
+        <UserAvatar />
+        <HeaderMenu />
+      </RightContainer>
     </StyledHeader>
   );
 }
