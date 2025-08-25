a15b7d2540ef2e0ae7f24f47ae361f129d2bd5f5
ShahdRaafat <shahdraafat.725@gmail.com>
2025-08-25 19:33:08 +0300

    feat: Add dashboard components implement statistics with data

---
+++ src/features/dashboard/DashboardLayout.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import { useCabins } from "../cabins/useCabins";
+import Spinner from "../../ui/Spinner";
+import Stats from "./Stats";
+const StyledDashboardLayout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: auto 34rem auto;
+  gap: 2.4rem;
+`;
+function DashboardLayout() {
+  const { bookings, isPending: isPendingBookings } = useRecentBookings();
+  const {
+    stays,
+    isPending: isPendingStays,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isPending: isPendingCabins } = useCabins();
+
+  if (isPendingBookings || isPendingStays || isPendingCabins)
+    return <Spinner />;
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>Today</div>
+      <div>Chart stay</div>
+      <div>chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
